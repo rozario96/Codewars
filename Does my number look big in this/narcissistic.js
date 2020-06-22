d886d6fc4e0c4068fac730ac6a129c91076607e9
Victor C. Rozario <victorroz@hotmail.com>
2020-06-22 21:25:17 +0600

    Does my number look big in this?

--- Does my number look big in this/narcissistic.js
+++ Does my number look big in this/narcissistic.js
@@ -1,11 +1,16 @@
 let narcissistic = (value) => {
-    let toString = value.toString();
-    let totalCount = 0;
+    let toString = value.toString(); // converting the given value to string
+
+    let totalCount = 0; // initializing counter to store summed up value
+
+    // looping through the string length to find the total count
     for (let i = 0; i < toString.length; i++) {
-        toInt = parseInt(toString[i], 10);
-        totalCount += Math.pow(toInt, toString.length);
+        toInt = parseInt(toString[i], 10); // converting the i-th character of the string to integer
+        totalCount += Math.pow(toInt, toString.length); // adding the value to the counter after performing power operation
     }
-    return totalCount === value ? true : false
+
+    // returning the validity of the number in the form of boolean
+    return totalCount === value ? true : false 
 } 
 
 console.log(narcissistic(15));
